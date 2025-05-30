9c467e5332f603b264d45f0c0c18c59afcb5127e
songok14 <rlathddhr11@gmail.com>
2025-05-30 17:12:58 +0900

    2025. 05. 30.(금)_2

--- 9. index.sql
+++ 9. index.sql
@@ -50,3 +50,61 @@ BEGIN
     END WHILE;
 END //
 DELIMITER ;
+
+-- 회원가입 및 주소생성
+DELIMITER //
+create procedure insert_author(in emailInput varchar(255), in nameInput varchar(255), in passwordInput varchar(255),in countryInput varchar(255), in cityInput varchar(255), in streetInput varchar(255))
+begin
+    declare exit handler for SQLEXCEPTION
+    begin
+        rollback;
+    end;
+    start transaction;
+    insert into author(email, name, password) values (emailInput, nameInput, passwordInput);
+    insert into address(author_id, country, city, street) values((select id from author order by id desc limit 1) , countryInput, cityInput, streetInput);
+    commit;
+end //
+DELIMITER ;
+
+-- 글쓰기
+DELIMITER //
+create procedure insert_post(in titleInput varchar(255), in contentsInput varchar(255), in emailInput varchar(255))
+begin
+    declare exit handler for SQLEXCEPTION
+    begin
+        rollback;
+    end;
+    start transaction;
+    insert into post(title, contents) values (titleInput, contentsInput);
+    insert author_post(author_id, post_id) values((select id from author where email=emailInput), (select id from post order by id desc limit 1));
+    commit;
+end //
+DELIMITER ;
+
+-- 글을 다른 유저가 편집할때
+바뀌게 될 제목과 내용을 update set 해준다. 그 때의 where 조건으로 post id를 가져와서 어떤 글인지를 명시해주어야 한다. (업데이트) 
+그 다음에 author_post 테이블에도 이메일 인풋을 입력해주고, 어떤 글인지 위에서 가져온 post id를 입력해준다. (추가) 
+
+-- 글편집하기
+DELIMITER //
+create procedure edit_post(in titleInput varchar(255), in contentsInput varchar(255), in emailInput varchar(255), in idInput bigint)
+begin
+    declare exit handler for SQLEXCEPTION
+    begin
+        rollback;
+    end;
+    start transaction;
+    update post set title=titleInput, contents=contentsInput where id=idInput;
+    insert author_post(author_id, post_id) values((select id from author where email=emailInput), idInput);
+    commit;
+end //
+DELIMITER ;
+
+-- 노션설명
+개념적 모델링 
+옵셔널 , mandatory 조건
+
+
+-- JOIN하여 데이터 조회
+select p.title as '제목', p.contents as '내용', a.name as '이름' from post p inner join author_post ap on p.id=ap.post_id
+inner join author a on a.id=ap.author_id;
